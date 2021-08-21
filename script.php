e5daed063e0c96ff70997ad8570dea76cc50581d
Philip <4144601+philipbrembeck@users.noreply.github.com>
2021-08-21 14:34:04 +0200

    Added product_name

--- script.php
+++ script.php
@@ -7,17 +7,19 @@ $barcode = $_GET['barcode'];
 
 $data = file_get_contents('https://world.openfoodfacts.org/api/v0/product/'.$barcode.'.json');
 
-$character = json_decode($data);
-$array = $character->product->ingredients_analysis_tags;
+$product = json_decode($data);
+
+$array = $product->product->ingredients_analysis_tags;
+$name = $product->product->product_name;
 
 if (in_array("en:non-vegan", $array)) {
-    echo "Not Vegan";
+    echo '"'.$name.'" is most likely not Vegan';
 }
 elseif (in_array("en:vegan-status-unknown", $array)) {
-    echo "We're not sure.";
+    echo  'We are not sure if "'.$name.'" is vegan or not.';
 }
 elseif (in_array("en:vegan", $array)) {
-	echo "Vegan!";
+	echo  '"'.$name.'" is Vegan!';
 }
 else {
 	echo "This product is not in our database yet.";
