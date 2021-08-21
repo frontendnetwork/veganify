779225fa2b6f7fcbe6741202db233054b69987d3
Philip <4144601+philipbrembeck@users.noreply.github.com>
2021-08-21 14:49:00 +0200

    Added JQuery for form

--- script.php
+++ script.php
@@ -3,25 +3,28 @@
 // Vegan uknown: https://world.openfoodfacts.org/api/v0/product/4008638170726.json
 // Not vegan: 5000159404259
 
-$barcode = $_GET['barcode'];
+$barcode = $_POST['barcode'];
 
-$data = file_get_contents('https://world.openfoodfacts.org/api/v0/product/'.$barcode.'.json');
-
-$product = json_decode($data);
-
-$array = $product->product->ingredients_analysis_tags;
-$name = $product->product->product_name;
-
-if (in_array("en:non-vegan", $array)) {
-    echo '"'.$name.'" is most likely not Vegan';
+if (empty($barcode)){
+	echo "Please enter a valid barcode.";
 }
-elseif (in_array("en:vegan-status-unknown", $array)) {
-    echo  'We are not sure if "'.$name.'" is vegan or not.';
-}
-elseif (in_array("en:vegan", $array)) {
-	echo  '"'.$name.'" is Vegan!';
-}
+
 else {
-	echo "This product is not in our database yet.";
+	$data = file_get_contents('https://world.openfoodfacts.org/api/v0/product/'.$barcode.'.json');
+	$product = json_decode($data);
+	$array = $product->product->ingredients_analysis_tags;
+	$name = $product->product->product_name;
+	if (in_array("en:non-vegan", $array)) {
+	    echo '"'.$name.'" is most likely not Vegan';
+	}
+	elseif (in_array("en:vegan-status-unknown", $array)) {
+	    echo  'We are not sure if "'.$name.'" is vegan or not.';
+	}
+	elseif (in_array("en:vegan", $array)) {
+		echo  '"'.$name.'" is Vegan!';
+	}
+	else {
+		echo "This product is not in our database yet.";
+	}
 }
 ?>
