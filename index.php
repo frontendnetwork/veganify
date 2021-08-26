3148e81a590972151b7494df97da564ae32e76bf
Philip <4144601+philipbrembeck@users.noreply.github.com>
2021-08-26 11:17:14 +0200

    Bumped to `v.0.1.0`

--- index.php
+++ index.php
@@ -19,7 +19,6 @@ https://github.com/philipbrembeck/vegancheck.me
     <link rel="apple-touch-icon" href="icon.png">
 
     <link href="css/style.min.css?v=3" rel="stylesheet">
-    <script src="js/jquery.min.js"></script>
   </head>
 
   <body>
@@ -27,21 +26,35 @@ https://github.com/philipbrembeck/vegancheck.me
       <div id="main">
         <img src="VeganCheck.svg" alt="Logo">
         <h1>VeganCheck.me</h1>
+        <p id="code"></p>
 
         <form action="script.php">
           <input type="text" id="barcode" name="barcode" placeholder="Enter Barcode of the product you'd like to check">
           <button name="submit"><i class="icon-right-open"></i></button>
         </form>
-
         <div id="result">&nbsp;</div>
         <footer>
           <p>Made with <i class="icon-love"></i> by <a href="https://philipbrembeck.com">Philip Brembeck</a>
-            <br>Hosted environmentally friendly / <a href="privacy-policy">Privacy Policy</a></p>
+            <br><a href="privacy-policy">Privacy Policy</a> / <a href="impressum">Imprint</a> / <a href="//github.com/sponsors/philipbrembeck/">Sponsor</a></p>
+            <a href="https://github.com/philipbrembeck/vegancheck.me"><img src="opensource.svg" alt="Open Source" class="labels"></a>
+            <a href="https://www.thegreenwebfoundation.org/green-web-check/?url=https%3A%2F%2Fvegancheck.me"><img src="greenhosted.svg" alt="Hosted Green" class="labels"></a>
         </footer>
       </div>
     </div>
 
  
+  <script src="js/jquery.min.js"></script>
+  <script src="js/BarcodeScanner.js"></script>
+  <script src="js/app.js"></script>
+  <script>
+  $(function() {      
+    let isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+    if (isMobile) {
+      setupLiveReader(resultElement)
+      var resultElement = document.getElementById('code')
+      }
+   });
+  </script>
   <script src="js/submit.js"></script>
 <?php 
         header('Access-Control-Allow-Origin: https://analytics.vegancheck.me'); 
