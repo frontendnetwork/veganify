bfb87144a8042b8b9bedf099265a09af44b2bdbb
Philip <4144601+philipbrembeck@users.noreply.github.com>
2021-08-31 00:34:39 +0200

    Added faster API-call, Added response for invalid codes

--- localization/script/de.php
+++ localization/script/de.php
@@ -4,10 +4,12 @@ if (empty($barcode)){
 }
 
 else {
-	$data = file_get_contents('https://world.openfoodfacts.org/api/v0/product/'.$barcode.'.json');
+	$data = file_get_contents('https://world.openfoodfacts.org/api/v0/product/'.$barcode);
 	$product = json_decode($data);
 	$array = $product->product->ingredients_analysis_tags;
 	$name = $product->product->product_name;
+	$response = $product->status_verbose;
+
 	if (in_array("en:non-vegan", $array)) {
 	    echo '<span class="non-vegan animated fadeIn">"'.$name.'" ist nicht vegan.</span>';
 	}
@@ -15,7 +17,10 @@ else {
 	    echo  '<div class="animated fadeIn"><span class="unknown">Wir wissen leider nicht ob "'.$name.'" vegan ist.</span><br><p class="unknown">Weißt du es? <a href="https://world.openfoodfacts.org/cgi/product.pl?type=edit&code='.$barcode.'">Ändere das Produkt bei OpenFoodFacts</a>.</p></div>';
 	}
 	elseif (in_array("en:vegan", $array)) {
-		echo  '<span class="vegan animated fadeIn">"'.$name.'" ist Vegan!</span>';
+		echo '<span class="vegan animated fadeIn">"'.$name.'" ist Vegan!</span>';
+	}
+	elseif ($response == "no code or invalid code"){
+		echo '<div class="animated fadeIn"><span class="missing">Mit diesem Barcode stimmt etwas nicht.</span></div>';
 	}
 	else {
 		echo '<div class="animated fadeIn"><span>Dieses Produkt haben wir noch nicht in unserer Datenbank.</span><p class="missing">Willst du es hinzufügen? <a href="https://world.openfoodfacts.org/cgi/product.pl?code='.$barcode.'">Dieses Produkt hinzufügen bei OpenFoodFacts</a>.</p></div>';
