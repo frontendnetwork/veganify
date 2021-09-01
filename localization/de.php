cbdb4167c2ed3a339b5a2cf9b6186e3320682945
Philip <87610380+gitgauner@users.noreply.github.com>
2021-09-01 23:08:46 +0200

    Changed `input=text` to `input=number`

--- localization/de.php
+++ localization/de.php
@@ -25,7 +25,7 @@ https://github.com/jokenetwork/vegancheck.me
     <meta name="msapplication-starturl" content="/">
     <meta name="theme-color" content="#000">
 
-    <link href="css/style.min.css?v=3" rel="stylesheet">
+    <link href="css/style.min.css?v=3.1" rel="stylesheet">
   </head>
 
   <body>
@@ -38,7 +38,7 @@ https://github.com/jokenetwork/vegancheck.me
         <form action="script.php">
           <fieldset>
             <span class="btn_scan" onclick="setupLiveReader()" aria-label="Barcode scannen" role="button"><i class="icon-barcode"></i></span>
-            <input type="text" id="barcode" name="barcode" placeholder="Barcode eingeben"> 
+            <input type="number" id="barcode" name="barcode" placeholder="Barcode eingeben" autofocus> 
             <input type="hidden" id="lang" name="lang" value="de">
             <button name="submit" aria-label="Absenden" role="button"><i class="icon-right-open"></i></button>
           </fieldset>
