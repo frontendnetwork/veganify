04cc1fa25d03a413226a3365dabedc257d214555
Philip <4144601+philipbrembeck@users.noreply.github.com>
2021-09-02 11:17:59 +0200

    Added tweet button

--- localization/script/de.php
+++ localization/script/de.php
@@ -11,13 +11,13 @@ else {
 	$response = $product->status_verbose;
 
 	if (in_array("en:non-vegan", $array)) {
-	    echo '<span class="non-vegan animated fadeIn">"'.$name.'" ist nicht vegan.</span>';
+	    echo '<div class="animated fadeIn"><span class="non-vegan">"'.$name.'" ist nicht vegan.</span><a href="https://twitter.com/intent/tweet?url=https://vegancheck.me&text='.$name.'%20ist%20nicht%20vegan!%20-%20Geprüft%20mit%20https://vegancheck.me" class="btn-dark">Tweet</a></div>';
 	}
 	elseif (in_array("en:vegan-status-unknown", $array)) {
 	    echo  '<div class="animated fadeIn"><span class="unknown">Wir wissen leider nicht ob "'.$name.'" vegan ist.</span><br><p class="unknown">Weißt du es? <a href="https://world.openfoodfacts.org/cgi/product.pl?type=edit&code='.$barcode.'">Ändere das Produkt bei OpenFoodFacts</a>.</p></div>';
 	}
 	elseif (in_array("en:vegan", $array)) {
-		echo '<span class="vegan animated fadeIn">"'.$name.'" ist Vegan!</span>';
+		echo '<div class="animated fadeIn"><span class="vegan">"'.$name.'" ist Vegan!</span><a href="https://twitter.com/intent/tweet?url=https://vegancheck.me&text='.$name.'%20ist%20vegan!%20-%20Geprüft%20mit%20" class="btn-dark">Tweet</a></div>';
 	}
 	elseif ($response == "no code or invalid code"){
 		echo '<div class="animated fadeIn"><span class="missing">Mit diesem Barcode stimmt etwas nicht.</span></div>';
