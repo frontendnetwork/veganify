2d991ec67a217211adfcff42244fb791852505ac
Philip <4144601+philipbrembeck@users.noreply.github.com>
2021-09-02 20:47:29 +0200

    Removed french translation for now

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-  $supportedLanguages=['en','de', 'fr'];
+  $supportedLanguages=['en','de'];
   if(!in_array($lang,$supportedLanguages)){
      $lang='en';
   }
