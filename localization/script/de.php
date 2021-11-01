7c894dd00764ea3dc2b13d646cfdc4fc7783ae6a
Philip <4144601+philipbrembeck@users.noreply.github.com>
2021-11-01 11:02:39 +0100

    Added button to edit product on OpenFoodFacts (Experimental)
    Co-Authored-By: Pierre Slamich <pierre@openfoodfacts.org>
    Co-Authored-By: Henrik <721721+dwarfex@users.noreply.github.com>

--- localization/script/de.php
+++ localization/script/de.php
@@ -13,10 +13,10 @@ else {
 		$response = $product->status_verbose;
 
 		if (in_array("en:non-vegan", $array)) {
-		    echo '<div class="animated fadeIn"><span class="non-vegan">"'.$name.'" ist nicht vegan.</span><a href="https://twitter.com/intent/tweet?url=https://vegancheck.me&text='.$name.'%20ist%20nicht%20vegan!%20-%20Geprüft%20mit%20https://vegancheck.me" class="btn-dark">Tweet</a></div>';
+		    echo '<div class="animated fadeIn"><span class="non-vegan">"'.$name.'" ist nicht vegan.</span><a href="https://twitter.com/intent/tweet?url=https://vegancheck.me&text='.$name.'%20ist%20nicht%20vegan!%20-%20Geprüft%20mit%20" class="btn-dark">Tweet</a></div>';
 		}
 		elseif (in_array("en:vegan-status-unknown", $array)) {
-		    echo  '<div class="animated fadeIn"><span class="unknown">Wir wissen leider nicht ob "'.$name.'" vegan ist.</span><br><p class="unknown">Weißt du es? <a href="https://world.openfoodfacts.org/cgi/product.pl?type=edit&code='.$barcode.'">Ändere das Produkt bei OpenFoodFacts</a>.</p></div>';
+		    echo  '<div class="animated fadeIn"><span class="unknown">Wir wissen leider nicht ob "'.$name.'" vegan ist.</span><br><a href="https://world.openfoodfacts.org/cgi/product.pl?type=edit&code='.$barcode.'" class="btn-dark">Produkt ändern</a></div>';
 		}
 		elseif (in_array("en:vegan", $array)) {
 			echo '<div class="animated fadeIn"><span class="vegan">"'.$name.'" ist Vegan!</span><a href="https://twitter.com/intent/tweet?url=https://vegancheck.me&text='.$name.'%20ist%20vegan!%20-%20Geprüft%20mit%20" class="btn-dark">Tweet</a></div>';
--- localization/script/en.php
+++ localization/script/en.php
@@ -17,7 +17,7 @@ else {
 		    echo '<div class="animated fadeIn"><span class="non-vegan">"'.$name.'" is not vegan.</span><a href="https://twitter.com/intent/tweet?url=https://vegancheck.me&text='.$name.'%20is%20not%20vegan!%20-%Checked%with%20" class="btn-dark">Tweet</a></div>';
 		}
 		elseif (in_array("en:vegan-status-unknown", $array)) {
-		    echo  '<div class="animated fadeIn"><span class="unknown">We are not sure if "'.$name.'" is vegan or not.</span><br><p class="unknown">Do you know? <a href="https://world.openfoodfacts.org/cgi/product.pl?type=edit&code='.$barcode.'">Edit this product at OpenFoodFacts</a>.</p></div>';
+		    echo  '<div class="animated fadeIn"><span class="unknown">We are not sure if "'.$name.'" is vegan or not.</span><br><a href="https://world.openfoodfacts.org/cgi/product.pl?type=edit&code='.$barcode.'" class="btn-dark">Edit product</a></div>';
 		}
 		elseif (in_array("en:vegan", $array)) {
 			echo '<div class="animated fadeIn"><span class="vegan">"'.$name.'" is vegan!</span><a href="https://twitter.com/intent/tweet?url=https://vegancheck.me&text='.$name.'%20is%20vegan!%20-%Checked%20with%20" class="btn-dark">Tweet</a></div>';
