4db289cd75d9d8998b10f7dac79da607de24c1df
Philip <4144601+philipbrembeck@users.noreply.github.com>
2021-11-04 14:04:43 +0100

    Fixed #1 (Added Palm-Oil info) [German only]

--- localization/script/de.php
+++ localization/script/de.php
@@ -1,4 +1,5 @@
 <?php
+
 if (empty($barcode)){
 	echo '<span class="animated fadeIn">Das Barcode-Feld darf nicht leer sein und keine Sonderzeichen enthalten.</span>';
 }
@@ -12,21 +13,29 @@ else {
 		$name = $product->product->product_name;
 		$response = $product->status_verbose;
 
-		if (in_array("en:non-vegan", $array)) {
-		    echo '<div class="animated fadeIn"><span class="non-vegan">"'.$name.'" ist nicht vegan.</span><a href="https://twitter.com/intent/tweet?url=https://vegancheck.me&text='.$name.'%20ist%20nicht%20vegan!%20-%20Geprüft%20mit%20" class="btn-dark">Tweet</a></div>';
+		if (in_array("en:palm-oil", $array)) {
+			$palmoil = '<span class="non-vegan"> Enthält Palmöl <span class="icon-cancel"></span></span>';
 		}
-		elseif (in_array("en:vegan-status-unknown", $array)) {
-		    echo  '<div class="animated fadeIn"><span class="unknown">Wir wissen leider nicht ob "'.$name.'" vegan ist.</span><br><a href="https://world.openfoodfacts.org/cgi/product.pl?type=edit&code='.$barcode.'" class="btn-dark">Produkt ändern</a></div>';
-		}
-		elseif (in_array("en:vegan", $array)) {
-			echo '<div class="animated fadeIn"><span class="vegan">"'.$name.'" ist Vegan!</span><a href="https://twitter.com/intent/tweet?url=https://vegancheck.me&text='.$name.'%20ist%20vegan!%20-%20Geprüft%20mit%20" class="btn-dark">Tweet</a></div>';
-		}
-		elseif ($response == "no code or invalid code"){
-			echo '<div class="animated fadeIn"><span class="missing">Mit diesem Barcode stimmt etwas nicht.</span></div>';
-		}
+
 		else {
-			echo '<div class="animated fadeIn"><span>Dieses Produkt haben wir noch nicht in unserer Datenbank.</span><p class="missing">Willst du es hinzufügen? <a href="https://world.openfoodfacts.org/cgi/product.pl?code='.$barcode.'">Dieses Produkt hinzufügen bei OpenFoodFacts</a>.</p></div>';
+			$palmoil = '<span class="vegan"> Kein Palmöl <span class="icon-ok"></span> </span>';
 		}
+
+			if (in_array("en:non-vegan", $array)) {
+			    echo '<div class="animated fadeIn"><span class="non-vegan">"'.$name.'" Vegan <span class="icon-cancel"></span> </span>'.$palmoil.'<a href="https://twitter.com/intent/tweet?url=https://vegancheck.me&text='.$name.'%20ist%20nicht%20vegan!%20-%20Geprüft%20mit%20" class="btn-dark">Tweet</a></div>';
+			}
+			elseif (in_array("en:vegan-status-unknown", $array)) {
+			    echo  '<div class="animated fadeIn"><span class="unknown">Wir wissen nicht ob "'.$name.'" vegan ist <span class="icon-cancel"></span> </span>'.$palmoil.'<br><a href="https://world.openfoodfacts.org/cgi/product.pl?type=edit&code='.$barcode.'" class="btn-dark">Produkt ändern</a></div>';
+			}
+			elseif (in_array("en:vegan", $array)) {
+				echo '<div class="animated fadeIn"><span class="vegan">"'.$name.'" ist Vegan <span class="icon-ok"></span> </span>'.$palmoil.'<a href="https://twitter.com/intent/tweet?url=https://vegancheck.me&text='.$name.'%20ist%20vegan!%20-%20Geprüft%20mit%20" class="btn-dark">Tweet</a></div>';
+			}
+			elseif ($response == "no code or invalid code"){
+				echo '<div class="animated fadeIn"><span class="missing">Mit diesem Barcode stimmt etwas nicht.</span></div>';
+			}
+			else {
+				echo '<div class="animated fadeIn"><span>Dieses Produkt haben wir noch nicht in unserer Datenbank.</span><p class="missing">Willst du es hinzufügen? <a href="https://world.openfoodfacts.org/cgi/product.pl?code='.$barcode.'">Dieses Produkt hinzufügen bei OpenFoodFacts</a>.</p></div>';
+			}
 	}
 	else {
 		echo '<div class="animated fadeIn"><span>Dieses Produkt haben wir noch nicht in unserer Datenbank.</span><p class="missing">Willst du es hinzufügen? <a href="https://world.openfoodfacts.org/cgi/product.pl?code='.$barcode.'">Dieses Produkt hinzufügen bei OpenFoodFacts</a>.</p></div>';
