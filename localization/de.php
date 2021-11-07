d9bbbfd846e3e4355034e2e0a2446f953e6d1c1b
Philip <4144601+philipbrembeck@users.noreply.github.com>
2021-11-07 14:35:33 +0100

    Bumped style version to `v3.9`

--- localization/de.php
+++ localization/de.php
@@ -30,7 +30,7 @@ https://github.com/jokenetwork/vegancheck.me
 
     <link rel="apple-touch-startup-image" href="img/iossplash.png">
 
-    <link href="css/style.min.css?v=3.8" rel="stylesheet">
+    <link href="css/style.min.css?v=3.9" rel="stylesheet">
   </head>
 
   <body>
