127f0f8126fb83a097f60e79cf3abaa104593e45
Philip Jake <philip@brembeck.me>
2021-11-27 16:57:16 +0100

    Update template.php

--- localization/script/template.php
+++ localization/script/template.php
@@ -1,6 +1,5 @@
 <!-- Thank you for helping translating VeganCheck.me!
 
-Please note: Please only translate text which is marked with ⬅ and don't translate any classes/ids.
 After translating, please delete any comments (like this one).
 
 Please don't forget to also translate the frontpage which can be found at localization/template.php
@@ -8,38 +7,23 @@ Please don't forget to also translate the frontpage which can be found at locali
 -->
 
 <?php
-if (empty($barcode)){
-	echo '<span class="animated fadeIn">Barcode-field cannot be empty or include special characters.</span>'; // ⬅ 
-}
+// Translated by: 
 
-else {
-	$data = file_get_contents('https://world.openfoodfacts.org/api/v0/product/'.$barcode);
-	$product = json_decode($data);
+$invalidrequest = 'Barcode cannot be empty or include special characters.';
+$invalidscan = 'This barcode is invalid.';
+$notindb = 'This product is not in our database yet.';
+$addit = 'Do you want to add it?';
+$addonoff = 'Add this product at OpenFoodFacts';
+$missinginfo = 'We do not have enough info on this product yet.';
+$addinfo = 'Want to add info?';
+$editonoff = 'Edit this product at OpenFoodFacts';
 
-
-	if (!empty($product->product)) {
-		$array = $product->product->ingredients_analysis_tags;
-		$name = $product->product->product_name;
-		$response = $product->status_verbose;
-
-		if (in_array("en:non-vegan", $array)) {
-		    echo '<div class="animated fadeIn"><span class="non-vegan">"'.$name.'" is not vegan.</span><a href="https://twitter.com/intent/tweet?url=https://vegancheck.me&text='.$name.'%20is%20not%20vegan!%20-%Checked%with%20" class="btn-dark">Tweet</a></div>'; // ⬅
-		}
-		elseif (in_array("en:vegan-status-unknown", $array)) {
-		    echo  '<div class="animated fadeIn"><span class="unknown">We are not sure if "'.$name.'" is vegan or not.</span><br><a href="https://world.openfoodfacts.org/cgi/product.pl?type=edit&code='.$barcode.'" class="btn-dark">Edit product</a></div>'; // ⬅
-		}
-		elseif (in_array("en:vegan", $array)) {
-			echo '<div class="animated fadeIn"><span class="vegan">"'.$name.'" is vegan!</span><a href="https://twitter.com/intent/tweet?url=https://vegancheck.me&text='.$name.'%20is%20vegan!%20-%Checked%20with%20" class="btn-dark">Tweet</a></div>'; // ⬅
-		}
-		elseif ($response == "no code or invalid code"){
-			echo '<div class="animated fadeIn"><span class="missing">This barcode is invalid.</span></div>'; // ⬅
-		}
-		else {
-			echo '<div class="animated fadeIn"><span>This product is not in our database yet.</span><p class="missing">Do you want to add it? <a href="https://world.openfoodfacts.org/cgi/product.pl?code='.$barcode.'">Add this product at OpenFoodFacts</a>.</p></div>'; // ⬅
-		}
-	}
-	else {
-		echo '<div class="animated fadeIn"><span>This product is not in our database yet.</span><p class="missing">Do you want to add it? <a href="https://world.openfoodfacts.org/cgi/product.pl?code='.$barcode.'">Add this product at OpenFoodFacts</a>.</p></div>'; // ⬅
-	}
-}
+$unknown = 'unknown';
+$containspalmoil = 'Contains Palmoil';
+$nopalmoil = 'No Palmoil';
+$palmoilunknown = 'Palmoil unknown';
+$notvegan = 'Not vegan';
+$tweettext = '%20is%20not%20vegan!%20-%Checked%with%20'; // Please keep '%20' as spaces
+$tweettextvegan = '%20is%20vegan!%20-%Checked%20with%20'; // Please keep '%20' as spaces
+$edit = 'Edit';
 ?>
