9969ca79d43d00d92214876b73edd6afd388e27a
Philip <4144601+philipbrembeck@users.noreply.github.com>
2021-11-29 08:58:48 +0100

    [Experimental] Added dutch localization (nl)
    Co-Authored-By: DeepL <13254824+DeepL@users.noreply.github.com>

---
+++ localization/script/nl.php
@@ -0,0 +1,20 @@
+<?php
+$invalidrequest = "Het barcodeveld mag niet leeg zijn en mag geen speciale tekens bevatten.";
+$invalidscan = 'Er is iets mis met deze barcode.';
+$notindb = "We hebben dit product nog niet in onze database.";
+$addit = "Wilt u het toevoegen?";
+$addonoff = "Voeg dit product toe aan OpenFoodFacts";
+$missinginfo = "We hebben nog niet genoeg info over dit product.";
+$addinfo = "Wilt u info toevoegen?";
+$editonoff = "Bewerk dit product bij OpenFoodFacts";
+
+$vegan = 'Veganistisch';
+$unknown = 'onbekend';
+$containspalmoil = "Bevat palmolie";
+$nopalmoil = 'Geen palmolie';
+$palmoilunknown = "Palmoliegehalte onbekend";
+$notvegan = 'Niet veganistisch';
+$tweettext = '%20is%20niet%20veganistisch%20-%Getest%20met%20';
+$tweettextvegan = '%20is%20veganistisch!%20-%20Getest%20met%20';
+$edit = 'Bewerken';
+?>
