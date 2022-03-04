7922d629b2b85fcf0d6e4614c2c4961d7874175c
Philip Jake <philip@cldsi.de>
2022-03-04 22:38:39 +0100

    Fully implemented `i18n.class.php` [#117]

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require('vendor/autoload.php');
-$i18n = new i18n('/var/www/virtual/jake/vegancheck.me/l10n/{LANGUAGE}.json', '/var/www/virtual/jake/vegancheck.me/langcache/', 'en');
+$i18n = new i18n('l10n/{LANGUAGE}.json', 'langcache/', 'en');
 $i18n->init();
 $supportedLanguages=['en','de', 'fr', 'es', 'nl'];
 $lang = $i18n->getAppliedLang();
