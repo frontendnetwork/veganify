db1437931154d44cc75a8459c45508e111821da4
Philip Jake <philip@cldsi.de>
2022-03-13 01:37:25 +0100

    Added prettified Lebensmittelwarnungen JSON [#115]

---
+++ includes/lebensmittelwarnung.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * function prettyPrint JSON
+ * @author Kendall Hopkins
+ * @uri https://stackoverflow.com/a/9776726/16673108
+ * @license CC-BY-SA 3.0
+ */
+
+function prettyPrint( $json )
+{
+    $result = '';
+    $level = 0;
+    $in_quotes = false;
+    $in_escape = false;
+    $ends_line_level = NULL;
+    $json_length = strlen( $json );
+
+    for( $i = 0; $i < $json_length; $i++ ) {
+        $char = $json[$i];
+        $new_line_level = NULL;
+        $post = "";
+        if( $ends_line_level !== NULL ) {
+            $new_line_level = $ends_line_level;
+            $ends_line_level = NULL;
+        }
+        if ( $in_escape ) {
+            $in_escape = false;
+        } else if( $char === '"' ) {
+            $in_quotes = !$in_quotes;
+        } else if( ! $in_quotes ) {
+            switch( $char ) {
+                case '}': case ']':
+                    $level--;
+                    $ends_line_level = NULL;
+                    $new_line_level = $level;
+                    break;
+
+                case '{': case '[':
+                    $level++;
+                case ',':
+                    $ends_line_level = $level;
+                    break;
+
+                case ':':
+                    $post = " ";
+                    break;
+
+                case " ": case "\t": case "\n": case "\r":
+                    $char = "";
+                    $ends_line_level = $new_line_level;
+                    $new_line_level = NULL;
+                    break;
+            }
+        } else if ( $char === '\\' ) {
+            $in_escape = true;
+        }
+        if( $new_line_level !== NULL ) {
+            $result .= "\n".str_repeat( "\t", $new_line_level );
+        }
+        $result .= $char.$post;
+    }
+
+    return $result;
+}
+
+header('Content-Type: application/json; charset=utf-8');
+$data = file_get_contents('warnings.json');
+echo prettyPrint($data); 
+die();
+
+?>
