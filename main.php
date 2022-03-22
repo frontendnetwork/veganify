8ce12a7ce950688cfe5d2fe837aa614ea27197ea
Philip Jake <philip@cldsi.de>
2022-03-22 22:36:06 +0100

    Added PWA Prompt again
    Co-Authored-By: Jacob Bearce <JacobDB@users.noreply.github.com>

--- main.php
+++ main.php
@@ -39,6 +39,7 @@ https://github.com/jokenetwork/vegancheck.me
     <link rel="apple-touch-startup-image" href="../img/iossplash.png?v=1.0.0">
 
     <link href="../css/style.min.css?v=3.9.65" rel="stylesheet">
+    <link href="../node_modules/pwa-install-prompt/style.css" rel="stylesheet">
   </head>
 
   <body id="top">
@@ -111,9 +112,27 @@ https://github.com/jokenetwork/vegancheck.me
   <div id="background"></div>
 </div>
 
+<div class="pwa-install-prompt__container">
+            <button class="pwa-install-prompt__overlay"><?php echo L::prompt_close; ?></button>
+            <div class="pwa-install-prompt">
+                <div class="pwa-install-prompt__icon__container">
+                    <img class="pwa-install-prompt__icon" src="../img/icon.png" alt="VeganCheck.me">
+                </div>
+                <div class="pwa-install-prompt__content">
+                    <h3 class="pwa-install-prompt__title"><?php echo L::prompt_install; ?></h3>
+                    <p class="pwa-install-prompt__text"><?php echo L::prompt_desc; ?></p>
+                    <p class="pwa-install-prompt__guide"><?php echo L::prompt_info; ?></p>
+                </div>
+            </div>
+        </div>
+
 <script src="../js/jquery.min.js"></script>
 <script src="../js/BarcodeScanner.min.js"></script>
 <script src="../js/app.js?v=1.0.16"></script>
+<script src="../node_modules/pwa-install-prompt/script.js"></script>
+<script>
+var prompt = new pwaInstallPrompt();
+</script>
 <?php 
         if (isset($_COOKIE['log']) && $_COOKIE['log'] == "Yes"){
           print_r('<script async src="https://analytics.vegancheck.me/ackee.js" data-ackee-server="https://analytics.vegancheck.me" data-ackee-domain-id="77898809-adfe-4573-a05f-88cd663f0fb5" data-ackee-opts=\'{ "detailed": true }\'></script>');
