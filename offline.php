1c8a69872053837445275a983156e49f1c1f0d8d
Philip Jake <philip@cldsi.de>
2022-10-31 13:14:29 +0100

    Added new styles to `offline`-page

--- offline.php
+++ offline.php
@@ -3,14 +3,12 @@
 ?>
   </head>
   <body>
-    <div class="container">
+    <div class="container top">
       <div id="main">
-        <img src="../img/VeganCheck.svg" alt="Logo">
-        <h1>VeganCheck.me</h1>
-        <p id="code"></p>
-
-        <h3><?php echo L::other_offline; ?></h3>
-        <h3><a href="/"><?php echo L::other_reload; ?></a></h3>
+        <div class="form">
+          <a href="//vegancheck.me"><img src="img/VeganCheck.svg" alt="Logo"></a><br>
+          <h3><?php echo L::other_offline; ?></h3>
+          <p><a href="/"><?php echo L::other_reload; ?></a></p>
       </div>
     </div>
     <script>
