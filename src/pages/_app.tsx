7f87442d77ba6038361da19e776fbbe7b325eb17
Philip <4144601+philipbrembeck@users.noreply.github.com>
2023-02-14 20:34:38 +0100

    Added Ackee (#157)
    Closes #157

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import "@/styles/style.scss";
 import type { AppProps } from "next/app";
 import { NextIntlProvider } from "next-intl";
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <>
     <NextIntlProvider messages={pageProps.messages}>
+      <Script async src="https://analytics.vegancheck.me/ackee.js" data-ackee-server="https://analytics.vegancheck.me" data-ackee-domain-id="77898809-adfe-4573-a05f-88cd663f0fb5" data-ackee-opts='{ "detailed": true }' />
       <Component {...pageProps} />
     </NextIntlProvider>
+    </>
   );
 }
