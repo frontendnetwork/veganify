bbdb17290d8c3906919ce7816fad6e50c64ce77d
Philip <4144601+philipbrembeck@users.noreply.github.com>
2023-05-05 16:32:43 +0200

    chore: Updated tests

--- src/tests/usability.spec.ts
+++ src/tests/usability.spec.ts
@@ -3,23 +3,23 @@ import { test, expect } from "@playwright/test";
 test("User should be able to load page and change language", async ({
   page,
 }) => {
-  await page.goto("http://localhost:1030/en");
+  await page.goto("http://localhost:3000/en");
   await page.click("text=More");
-  await expect(page).toHaveURL("http://localhost:1030/more");
+  await expect(page).toHaveURL("http://localhost:3000/more");
   await page.click("text=Language");
   await page.click("text=German");
-  await expect(page).toHaveURL("http://localhost:1030/de/more");
+  await expect(page).toHaveURL("http://localhost:3000/de/more");
 });
 
 test("User should be able to input a barcode and get a result", async ({
   page,
 }) => {
-  await page.goto("http://localhost:1030/en");
+  await page.goto("http://localhost:3000/en");
 
   const inputField = await page.$('input[name="barcode"]');
-  await inputField.type("4066600204404");
+  await inputField?.type("4066600204404");
   const submitButton = await page.$('button[name="submit"]');
-  await submitButton.click();
+  await submitButton?.click();
 
   await page.waitForSelector(".loading_skeleton", { state: "hidden" });
 
@@ -30,12 +30,12 @@ test("User should be able to input a barcode and get a result", async ({
 test("User should be able to input ingredients and get a result", async ({
   page,
 }) => {
-  await page.goto("http://localhost:1030/en/ingredients");
+  await page.goto("http://localhost:3000/en/ingredients");
 
   const inputField = await page.$('textarea[id="ingredients"]');
-  await inputField.type("Duck");
+  await inputField?.type("Duck");
   const submitButton = await page.$('button[name="checkingredients"]');
-  await submitButton.click();
+  await submitButton?.click();
 
   await page.waitForSelector(".loading_skeleton", { state: "hidden" });
   const resultText = await page.textContent(".resultborder");
@@ -65,11 +65,11 @@ test("User should be able to input ingredients and get a result", async ({
 test("User should be able to switch on OLED mode in darkmode, in lightmode, user should get an error", async ({
   page,
 }) => {
-  await page.goto("http://localhost:1030/en/more");
+  await page.goto("http://localhost:3000/en/more");
 
   // Locate the switch input and click on it
   const switchInput = await page.$("#oled-switch");
-  await switchInput.click();
+  await switchInput?.click();
 
   // Wait for either the "animated shake" class (error) or the background color to change
   await Promise.any([
@@ -91,7 +91,7 @@ test("User should be able to switch on OLED mode in darkmode, in lightmode, user
     expect(bodyStyles.color).toBe("#141414");
   } else {
     // Light mode was activated
-    const switchClasses = await switchInput.evaluate((input) =>
+    const switchClasses = await switchInput?.evaluate((input) =>
       Array.from(input.classList)
     );
     expect(switchClasses).toEqual(
@@ -131,7 +131,7 @@ test("User should be able to input a barcode via the URL parameter `ean` ", asyn
     });
   });
 
-  await page.goto("http://localhost:1030/en?ean=4066600204404");
+  await page.goto("http://localhost:3000/en?ean=4066600204404");
 
   const inputField = await page.waitForSelector('input[name="barcode"]', {
     visible: true,
