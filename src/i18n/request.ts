1ce98b3c1d5a34f75efdda4eb16ff58b4ea0e6e6
Philip <4144601+philipbrembeck@users.noreply.github.com>
2024-12-28 16:09:45 +0100

    fix: Exchange any type for routing.locales number

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -5,8 +5,10 @@ import { routing } from "./routing";
 export default getRequestConfig(async ({ requestLocale }) => {
   let locale = await requestLocale;
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  if (!locale || !routing.locales.includes(locale as any)) {
+  if (
+    !locale ||
+    !routing.locales.includes(locale as (typeof routing.locales)[number])
+  ) {
     locale = routing.defaultLocale;
   }
 
