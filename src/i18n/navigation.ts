7135fb50bd3d8fffc7f05775b763ca975eda7e33
Philip <4144601+philipbrembeck@users.noreply.github.com>
2025-10-28 17:15:22 +0100

    chore(deps): Upgrade to Next.js v16

--- src/i18n/navigation.ts
+++ src/i18n/navigation.ts
@@ -1,5 +1,4 @@
 import { createNavigation } from "next-intl/navigation";
-
 import { routing } from "./routing";
 
 export const { Link, redirect, usePathname, useRouter, getPathname } =
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,5 +1,4 @@
 import createMiddleware from "next-intl/middleware";
-
 import { routing } from "./i18n/routing";
 
 export default createMiddleware(routing);
